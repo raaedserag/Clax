b196e7333c0f02241d66855405e8d9fdc0b8030e
Raaed_Serag <raaedserag@gmail.com>
2020-02-10 20:08:33 +0200

    adding passengers model

---
+++ models/passengers.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+// Passengers Model
+const passengerSchema = new mongoose.Schema({
+  name: {type: String},
+  mail: {type: String},
+  pass: {type: String},
+  phone: {type: String},
+  rate: {type: Number},
+  balance: {type: Number},
+  maxLoan: {type: Number},
+  _currentTrip:{type: mongoose.ObjectId, ref: 'CurrentTrips'},
+  _pastTrips: [{type: mongoose.ObjectId, ref: 'PastTrips'}],
+  _complains: [{type: mongoose.ObjectId, ref: 'Complains'}]
+
+});
+const Passengers = mongoose.model("Passengers", passengerSchema);
+
+module.exports.Passengers = Passengers;
+
