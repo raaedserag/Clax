c4a81f9997b8e3bce184816e2b41eba7aad6f0d7
Raaed_Serag <raaedserag@gmail.com>
2020-02-13 08:46:57 +0200

    stations model fixing

--- models/stations.js
+++ models/stations.js
@@ -2,8 +2,19 @@ const mongoose = require("mongoose");
 
 // Station Model
 const stationSchema = new mongoose.Schema({
-    name: {type: String},
-    location: {type: {type:String}, coordinates: [{type: Number}]}
+    name: {
+        type: String, 
+        required: true,
+        lowercase: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 20
+    },
+
+    location: {
+        type: {type:String, required: true}, 
+        coordinates: [{type: Number, required: true}]
+    }
 });
 const Stations = mongoose.model("Stations", stationSchema);
 
