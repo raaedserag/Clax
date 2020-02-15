969347d57c1585fdad4f56bea11838d6518481ef
Raaed_Serag <raaedserag@gmail.com>
2020-02-15 18:34:38 +0200

    add validations to complains model

--- models/complains.js
+++ models/complains.js
@@ -1,17 +1,67 @@
+// Modules
 const mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
+Joi.objectId = require('joi-objectid')(Joi)
 
-// Complains Model
+
+//****************** Complain Model ******************
+// Schema
 const complainSchema = new mongoose.Schema({
-    text: {type: String},
-    date: {type: Date},
-    from_passenger: {type: Boolean},    // true means: complain from passenger about a driver, vice versa
-    _trip: {type: mongoose.ObjectId, ref: 'PastTrips'},
-    status: {type: String, 
+    title:{
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 4,
+        maxlength: 32 
+    },
+    text: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 4,
+        maxlength: 500
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    from_passenger: {
+        type: Boolean,
+        default: true
+    },
+    status: {
+        type: String, 
         enum: ['pending', 'taken', 'resolved'], 
         default: 'pending'
         },
+    _trip: {type: mongoose.ObjectId, ref: 'PastTrips'}
+    
     
 });
-const Complains = mongoose.model("Complains", complainSchema);
 
-module.exports.Complains = Complains;
+
+////****************** Complain Validation  ******************
+// Set Validation Schema 
+const validationSchema = Joi.object().keys({
+    title: Joi.string()
+    .required()
+    .trim()
+    .min(4)
+    .max(32),
+    text: Joi.string()
+    .required()
+    .trim()
+    .min(4)
+    .max(500),
+    date: Joi.date(),
+    status: Joi.string(),
+    //.enum(['pending', 'taken', 'resolved']),
+    _trip: Joi.objectId(),
+  });
+
+const validateComplain =function(complain){
+    return validationSchema.validate(complain);
+};
+  
+module.exports.Complains = mongoose.model("Complains", complainSchema);
+module.exports.validateComplain = validateComplain;
