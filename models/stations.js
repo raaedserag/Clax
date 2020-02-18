71adb12f13ebe5033366972d8b24dbc71a595974
Raaed_Serag <raaedserag@gmail.com>
2020-02-18 18:27:37 +0200

    Finishing stations-model

--- models/stations.js
+++ models/stations.js
@@ -1,21 +1,54 @@
 const mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
 
-// Station Model
+//****************** Stations Model ******************
+// Schema
 const stationSchema = new mongoose.Schema({
     name: {
         type: String, 
         required: true,
-        lowercase: true,
         trim: true,
         minlength: 3,
-        maxlength: 20
+        maxlength: 30
     },
-
-    location: {
-        type: {type:String, required: true}, 
-        coordinates: [{type: Number, required: true}]
+    description: {
+        type: String,
+        default: null,
+        maxLength: 120
+    },
+    type: {
+        type:String, 
+        required: true,
+        enum:['Point', 'Polygon']
+    }, 
+    coordinates: {
+        type: Array,
+        required: true,
+        set: function(c){
+            if (this.type == 'Point') return c[0][0]
+            else return c
+        }
     }
 });
-const Stations = mongoose.model("Stations", stationSchema);
 
-module.exports.Stations = Stations;
+////****************** Station Validation  ******************
+// Set Validation Schema
+const validationSchema = Joi.object().keys({
+    name: Joi.string()
+    .required()
+    .trim()
+    .min(3)
+    .max(30),
+    description: Joi.string()
+    .max(120),
+    type: Joi.string()
+    .required()
+    .valid('Point', 'Polygon'),
+    coordinates: Joi.array().items(Joi.array().items(Joi.array().items(Joi.number())))
+});
+const validateStation = function(station){
+    return validationSchema.validate(station);
+}
+
+module.exports.Stations = mongoose.model("Stations", stationSchema);
+module.exports.validateStation = validateStation;
