633635f33d3b1961e524be0b8d78c55fa5ec2583
Raaed <raaedserag@gmail.com>
2020-03-08 22:10:57 +0200

    Merging Routes

--- startup/routes.js
+++ startup/routes.js
@@ -1,31 +1,47 @@
-// Modules
+// Import Modules
 const express = require("express");
 const morgan = require("morgan");
+const startDebugger = require("debug")("app:start"); //create a 'start' debugger
+
+
+// Import Middlewares
+const error = require("../middlewares/error");
+
+
+// Import Routes
+const complaintRoute=require('../routes/complaints');
+const transRoute=require('../routes/transactions');
 const offers = require("../routes/offers");
 const passengers = require("../routes/passengers");
 const passengerLogin = require("../routes/passenger-login");
 const transaction = require("../routes/transactions");
 const pastTrips = require("../routes/past-trips");
 const family = require("../routes/family");
-// Middlewares
-const error = require("../middlewares/error");
 
-// Routes
-//----
+
 module.exports = function(app) {
-  // Apply Middlewares
-  app.use(express.json()); // Reparse body of the request into json object
-  app.use(express.urlencoded({ extended: true })); // Reparse url to encoded url payload
-  //app.use(express.static("public")); // For static files if needed
-  // in development mode only
-  if (app.get("env") === "development") {
-    app.use(morgan("tiny"));
-  }
-  app.use("/api/offers", offers);
-  app.use("/api/passengers", passengers);
-  app.use("/api/login", passengerLogin);
-  app.use("/api/transactions", transaction);
-  app.use("/api/past-trips", pastTrips);
-  app.use("/api/family", family);
-  app.use(error);
+    // Apply Essential Middlewares
+    app.use(express.json()); // Reparse body of the request into json object
+    app.use(express.urlencoded({ extended: true })); // Reparse url to encoded url payload
+    //app.use(express.static("public")); // For static files if needed
+    
+    // Apply MiddleWares
+    app.use(error);
+
+    // Apply Middlewares in development mode only
+    if(app.get('env') === 'development')
+    {
+        app.use(morgan("tiny"));
+        startDebugger('Morgan Enabled')
+    }
+
+    // Apply Routes
+    app.use("/api/user",complaintRoute);
+    app.use("/",transRoute);
+    app.use("/api/offers", offers);
+    app.use("/api/passengers", passengers);
+    app.use("/api/login", passengerLogin);
+    app.use("/api/transactions", transaction);
+    app.use("/api/past-trips", pastTrips);
+    app.use("/api/family", family);
 };
