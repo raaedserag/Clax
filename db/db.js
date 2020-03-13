bdaf396c63f2da35e6bb1fff71d3569ec5f53dab
Raaed <raaedserag@gmail.com>
2020-03-13 20:20:52 +0200

    moving files

--- db/db.js
+++ db/db.js
@@ -1,31 +1,21 @@
 //*****Modules*****
-const mongoose = require("mongoose");
-const dbDebugger = require("debug")("app:db");
+const mongoose = require('mongoose');
+const dbDebugger = require('debug')('app:db');
 const uri = require("../startup/config").connectionString();
 
 //Opening connection, debugged with app:db
 //Use => call it with async-await before read or write to database, the connection stills open till closing it.
-const connect = async function() {
-  await mongoose
-    .connect(uri, {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    })
-    .then(() => console.log("DB connected..."))
-    .catch(err => {
-      console.log(` DB connection failed: ${err} \n Reconnecting...`);
-      setTimeout(connect, 2000);
-    });
-};
-module.exports.connect = connect;
+module.exports.connect =()=>{
+    mongoose.connect(uri, {useNewUrlParser: true, useFindAndModify: false,
+    useUnifiedTopology: true, useCreateIndex: true})
+    .then(()=> dbDebugger('DB connected...'))
+    .catch((err) => dbDebugger('DB connection failed!!:\n', err));
+}
 
 //Closing connection, debugged with app:db
 //Use => call it with async-await after finishing read or write to database.
-module.exports.close = async () => {
-  await mongoose.connection
-    .close()
-    .then(() => console.log("DB closed..."))
-    .catch(err => console.log("DB clossing failed!!:\n", err));
-};
+module.exports.close = ()=>{
+    mongoose.connection.close()
+    .then(()=> dbDebugger('DB closed...'))
+    .catch((err) => dbDebugger('DB clossing failed!!:\n', err));
+}
