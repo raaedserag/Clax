0fef153c9d753a427a43c287309f2fd64afaa3e5
KareemAllam <36572428+KareemAllam@users.noreply.github.com>
2020-03-13 22:06:19 +0200

    Flutter Integration Bugs Fixed
    bugs fixed

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,70 +1,64 @@
-// Modules 
+// Modules
 const Joi = require("@hapi/joi");
 // Models
-const Passengers = require("../models/passengers-model").Passengers
+const Passengers = require("../models/passengers-model").Passengers;
 // Setup Error Debugger
 const errorDebugger = require("debug")("app:error");
 
 // Functions
 // Retreive use balance
-module.exports.getUserBalance = async function(userId){
-    try {
-        const user = await Passengers.findById(userId)
-        .select('-_id balance')
-        return {success: true, result: user}
-    } 
-    catch (error) {
-        errorDebugger(error.message)
-        return {success: false, result: error.message}
-    }
-}
+module.exports.getUserBalance = async function(userId) {
+  try {
+    const user = await Passengers.findById(userId).select("-_id balance");
+    return { success: true, result: user };
+  } catch (error) {
+    errorDebugger(error.message);
+    return { success: false, result: error.message };
+  }
+};
 
-module.exports.getUserStripeId = async function(userId){
-    try {
-        const user = await Passengers.findById(userId)
-        .select('-_id stripeId')
-        return {success: true, result: user}
-    } 
-    catch (error) {
-        errorDebugger(error.message)
-        return {success: false, result: error.message}
-    }
-}
+module.exports.getUserStripeId = async function(userId) {
+  try {
+    const user = await Passengers.findById(userId).select("-_id stripeId");
+    return { success: true, result: user };
+  } catch (error) {
+    errorDebugger(error.message);
+    return { success: false, result: error.message };
+  }
+};
 
 // Validators
 // Card Schema Validator
-const cardSchema =  Joi.object().keys({
-    number: Joi.string()
-    .required(),
-    exp_month: Joi.number()
+const cardSchema = Joi.object().keys({
+  number: Joi.string().required(),
+  exp_month: Joi.number()
     .integer()
     .min(1)
     .max(12)
     .required(),
-    exp_year: Joi.number()
+  exp_year: Joi.number()
     .integer()
     .required(),
-    cvc: Joi.number()
+  cvc: Joi.number()
     .integer()
-    .required()
-    //Adding id shit, Muuuuuuust be removed soon
-    ,id: Joi.string().required()
+    .required(),
+  //Adding id shit, Muuuuuuust be removed soon
+  id: Joi.string().required()
 });
-module.exports.validateCard = function(card){
-    return cardSchema.validate(card);
+module.exports.validateCard = function(card) {
+  return cardSchema.validate(card);
 };
 
 // Charge Balance Schema
 const chargeSchema = Joi.object().keys({
-    amount: Joi.number()
+  amount: Joi.number()
     .integer()
     .min(1)
     .required(),
-    source: Joi.string()
-    .required()
-    //Adding id shit, Muuuuuuust be removed soon
-    ,id: Joi.string().required()
+  source: Joi.string().required(),
+  //Adding id shit, Muuuuuuust be removed soon
+  id: Joi.string().required()
 });
-module.exports.validateCharge = function(charge){
-    return chargeSchema.validate(charge);
+module.exports.validateCharge = function(charge) {
+  return chargeSchema.validate(charge);
 };
