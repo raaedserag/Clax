b826436a860fb09b426d2a82dd43a6e0839e862b
omarattallah97 <32816430+ertomar@users.noreply.github.com>
2020-04-23 11:47:36 +0200

    added dockerfile

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Modules
-const winston = require("winston")
+const winston = require("winston");
 const configuration = require("./startup/config");
 const express = require("express");
 const app = express();
@@ -18,4 +18,4 @@ require("./startup/routes")(app);
 
 // Initiate the server on the selected PORT
 const { host, port } = configuration.serverConfig();
-app.listen(port, host, () => winston.info(`Listening on port ${port}`));
+app.listen(port, () => winston.info(`Listening on port ${port}`));
