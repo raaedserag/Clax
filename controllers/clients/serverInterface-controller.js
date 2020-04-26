17060cb38a8d2dc09fe5dae13e4058ab75b6e6ba
Raaed <raaedserag@gmail.com>
2020-04-26 07:55:05 +0200

    Changing serverStatus statement

--- controllers/clients/serverInterface-controller.js
+++ controllers/clients/serverInterface-controller.js
@@ -8,7 +8,7 @@ const dbStatusDict = ["Disconnected", "Connected", "Connecting", "Disconnecting"
 module.exports.getServerStatus = async (req, res) => {
     // Render status web-page
     return res.send({
-        serverStatus: `Listening on ${req.header("host")}`,
+        serverStatus: `Hosting ${req.header("host")}`,
         dbType: dbType[0].toUpperCase() + dbType.slice(1),
         dbStatus: dbStatusDict[mongoose.connection.readyState]
     })
