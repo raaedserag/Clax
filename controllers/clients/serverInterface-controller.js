fcb9c8c627a1ed12b749f029b8b1169e9f769c0b
Raaed <raaedserag@gmail.com>
2020-05-09 09:43:41 +0200

    optimize

--- controllers/clients/serverInterface-controller.js
+++ controllers/clients/serverInterface-controller.js
@@ -2,12 +2,12 @@
 const path = require('path');
 const mongoose = require('mongoose');
 // Configuration
-const { dbType } = require("../../startup/config").dbConfig();["Disconnected", "Connected", "Connecting", "Disconnecting"]
+const { dbType } = require("../../startup/config").dbConfig();
 //----------------
 
 module.exports.getServerStatus = async (req, res) => {
     res.set({
-        'x-dbType': dbType[0].toUpperCase() + dbType.slice(1).toString(),
+        'x-dbType': dbType,
         'x-dbStatus': mongoose.connection.readyState.toString()
     }).sendFile(path.resolve(__dirname + '../../../views/index.html'));
 
