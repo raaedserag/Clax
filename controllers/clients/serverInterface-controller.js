b579d190e3af14a73b85f1b48457033d94bb880f
Raaed <raaedserag@gmail.com>
2020-05-09 08:54:49 +0200

    optimize dbType response

--- controllers/clients/serverInterface-controller.js
+++ controllers/clients/serverInterface-controller.js
@@ -2,14 +2,13 @@
 const path = require('path');
 const mongoose = require('mongoose');
 // Configuration
-const { dbType } = require("../../startup/config").dbConfig();
-const dbStatusDict = ["Disconnected", "Connected", "Connecting", "Disconnecting"]
+const { dbType } = require("../../startup/config").dbConfig();["Disconnected", "Connected", "Connecting", "Disconnecting"]
 //----------------
 
 module.exports.getServerStatus = async (req, res) => {
     res.set({
         'x-dbType': dbType[0].toUpperCase() + dbType.slice(1).toString(),
-        'x-dbStatus': dbStatusDict[mongoose.connection.readyState].toString()
+        'x-dbStatus': mongoose.connection.readyState.toString()
     }).sendFile(path.resolve(__dirname + '../../../views/index.html'));
 
 
