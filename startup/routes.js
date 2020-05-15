687d1d5bab815877069657b55d685298ccc0184b
omarattallah97 <32816430+ertomar@users.noreply.github.com>
2020-05-16 01:07:36 +0200

    mail verfication

--- startup/routes.js
+++ startup/routes.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 // Import Middlewares
 const error = require("../middlewares/error");
 const authentication = require("../middlewares/authentication");
+const webConfig = require("../middlewares/web-config");
 
 // Import Routes
 // Login & Registration
@@ -22,11 +23,11 @@ const paypal = require("../routes/payment/paypal-route");
 // Pairing & Tracking Section
 const pairing = require("../routes/pairing/pairing");
 // Externals Section
-const passengerExternal = require('../routes/clients/passengerExternal-route')
-
+const passengerExternal = require("../routes/clients/passengerExternal-route");
 
 module.exports = function (app) {
   // Apply Essential Middlewares
+  app.use(webConfig);
   app.use(express.json()); // Reparse body of the request into json object
   app.use(express.urlencoded({ extended: true })); // Reparse url to encoded url payload
   //app.use(express.static("public")); // For static files if needed
@@ -46,14 +47,17 @@ module.exports = function (app) {
   app.use("/api/passengers/past-trips", authentication, pastTripsRoute);
   // Payments & Complains Section
   app.use("/api/passengers/complains", authentication, complaintRoute);
-  app.use("/api/passengers/payments/manage-financials", authentication, manageFinancialsRoute);
+  app.use(
+    "/api/passengers/payments/manage-financials",
+    authentication,
+    manageFinancialsRoute
+  );
   app.use("/api/passengers/payments/loaning", authentication, loaningRoute);
   app.use("/api/passengers/paypal", paypal);
   // Pairing & Tracking Section
   app.use("/api/pairing", pairing);
   // Externals Section
-  app.use("/clients/passengers", passengerExternal)
-
+  app.use("/clients/passengers", passengerExternal);
 
   // Apply Error Middle ware
   app.use(error);
