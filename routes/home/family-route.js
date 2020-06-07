258987a6d41eab42635d464b0f133af0a44f6cba
omarattallah97 <32816430+ertomar@users.noreply.github.com>
2020-06-07 19:12:23 +0200

    added missing family route

--- routes/home/family-route.js
+++ routes/home/family-route.js
@@ -5,6 +5,7 @@ const {
   sendFamilyRequest,
   cancelFamilyRequest,
   deleteFamilyMember,
+  acceptRequest,
   denyRequest,
 } = require("../../controllers/home/family-controller");
 
@@ -12,6 +13,7 @@ router.get("/", getFamilyInfo);
 router.put("/add", sendFamilyRequest);
 router.put("/cancel", cancelFamilyRequest);
 router.put("/delete", deleteFamilyMember);
+router.put("/accept-request", acceptRequest);
 router.put("/deny-request", denyRequest);
 
 module.exports = router;
