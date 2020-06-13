d4e5a90c4ff35e2ff9e05c8a199e6bced66c5d40
somayamostafa <somayaelbaradey@gmail.com>
2020-06-13 16:46:30 +0200

    Get and Upate driver info

--- controllers/driverHome/settings.js
+++ controllers/driverHome/settings.js
@@ -1,25 +1,54 @@
-// Models
+//Models
 const { Drivers } = require("../../models/drivers-model");
+const { PastTrips } =  require('../../models/past-trips-model');
+const { Cars } =  require('../../models/cars-model');
 
-// Helpers
-const sms = require("../../services/nexmo-sms");
+//Helpers
 const { validateUpdateMe } = require("../../validators/settings-validator");
-const {
-    hashing
-  } = require("../../helpers/encryption-helper");
+const { encodeId, hashing } = require("../../helpers/encryption-helper");
 
-// Get driver info
-module.exports.getMe = async (req, res) => {
-    const driver = await Drivers.findById("5e5690b3e781d4395c633b44").select(
-      "-_id name mail phone pass passLength phone_verified mail_verified"
+  // Get avarege rate and driver info
+  module.exports.getDriver = async (req,res) => {
+
+    let rate = await PastTrips.find({
+      _driver: req.user._id,
+    }).select("-_id rate ");
+
+    rate = JSON.parse(JSON.stringify(rate));
+    function averageRate(list) {
+      let sum = 0;
+      let rateCount = 0;
+      for (let i = 0; i < list.length; i++) {
+          sum += list[i].rate;
+          rateCount++;
+      }
+      if (rateCount == 0) {
+        return 0; 
+      }
+      let avg = sum / rateCount;
+        return avg;
+    }   
+    
+    const data = await Drivers.findById(req.user._id).select(
+        "-_id name rate status.is_available phone profilePic tripsCount"
     );
-    return res.send(driver);
+
+    let avg = averageRate(rate);
+    let info = {
+      avg : avg.toString(),
+      data
+    }
+
+    await Drivers.findByIdAndUpdate(req.user._id, {
+      rate: avg.toString(),
+    });
+    return res.send(info);
   };
 
-// Update driver info
-module.exports.updateMe = async (req, res) => {
+  // update the driver info
+  module.exports.updateDriver = async (req, res) => {
     
-    // Validate update reuest
+    // Validate update request
     const { error, value } = validateUpdateMe(req.body);
     if (error) return res.status(404).send(error.details[0].message);
     let request = value;
@@ -30,15 +59,48 @@ module.exports.updateMe = async (req, res) => {
     if (request.pass) request.pass = await hashing(request.pass);
 
     if (request.firstName) {
-    let name = {};
-    name.first = request.firstName;
-    name.last = request.lastName;
-    delete request.lastName;
-    delete request.firstName;
-    request.name = name;
+      let name = {};
+      name.first = request.firstName;
+      name.last = request.lastName;
+      delete request.lastName;
+      delete request.firstName;
+      request.name = name;
     }
 
-    await Drivers.findByIdAndUpdate("5e5690b3e781d4395c633b44", request);
-
+    await Drivers.findByIdAndUpdate('5ee2490b9b6b5f4018c75f82', request);
+    console.log(request)
     return res.sendStatus(200);
-    };
+  };
+
+  //Add car
+  module.exports.addCar = async (req, res) => {
+    let isExisted = await Cars.findOne({ plateNumber: req.body.plateNumber });
+    if (!isExisted){
+      //Add 
+      let newCar={
+      plateNumber: req.body.plateNumber,
+      color: req.body.color,
+      license: {
+        copy: req.body.license.copy,
+        ownerName : req.body.license.ownerName,
+        vin: req.body.license.vin,
+        released:req.body.license.released,
+        checking:req.body.license.checking,
+        expires:req.body.license.expires
+      },
+      _line: "5e716d485797b82358227805"
+      }
+      newCar = await Cars.create(newCar);
+      let car = {
+        _cars : newCar._id
+      };
+      await Drivers.update({_id: "5ee2490b9b6b5f4018c75f82"}, {$push: car});
+    }
+
+    let car = {
+      _cars : isExisted._id
+    }
+    
+    await Drivers.update({_id: "5ee2490b9b6b5f4018c75f82"}, {$push: car});
+    return res.sendStatus(200);
+  };
