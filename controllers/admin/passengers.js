fe92f894bf58c2806856c01318038007e1261f01
omarattallah97 <32816430+ertomar@users.noreply.github.com>
2020-06-16 23:00:00 +0200

    added userr profile

--- controllers/admin/passengers.js
+++ controllers/admin/passengers.js
@@ -6,3 +6,27 @@ module.exports.getPassengers = async (req, res) => {
   const users = await Passengers.find().select("name phone balance rate");
   res.send(users);
 };
+module.exports.editPassengers = async (req, res) => {
+  res.send("users");
+};
+module.exports.getPassengerById = async (req, res) => {
+  let params = { id: req.params.id };
+  const schema = Joi.object().keys({
+    id: Joi.objectId().required(),
+  });
+  const { error } = schema.validate(params);
+  if (error) return res.status(400).send(error.details[0].message);
+  const users = await Passengers.findOne({ _id: req.params.id })
+    .populate([
+      {
+        path: "_pastTrips",
+        select: "_id",
+      },
+      {
+        path: "_family",
+        select: "name",
+      },
+    ])
+    .select("-pass");
+  res.send(users);
+};
