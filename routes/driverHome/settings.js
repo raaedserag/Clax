7dd0edbbc879772c210268ab8287923aef1b0a38
somayamostafa <somayaelbaradey@gmail.com>
2020-06-18 22:34:29 +0200

    modify update driver

--- routes/driverHome/settings.js
+++ routes/driverHome/settings.js
@@ -3,11 +3,22 @@ const router = require("express").Router();
 const {
     getDriver,
     updateDriver,
-    addCar
+    addCar,
+    requestMailVerification,
+    confirmMail,
+    requestPhoneVerification,
+    confirmPhone
 } = require("../../controllers/driverHome/settings");
 
 router.get("/", getDriver);
 router.put("/",updateDriver);
+
 router.post("/addCar",addCar);
+
+router.post("/mail-verification", requestMailVerification);
+router.put("/mail-verification", confirmMail);
+
+router.post("/phone-verification", requestPhoneVerification)
+router.put("/phone-verification", confirmPhone);
 
 module.exports = router;
