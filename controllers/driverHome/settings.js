b80b02ea5c3e0bb0b2c3d5ea4474350d5f347148
somayamostafa <somayaelbaradey@gmail.com>
2020-06-18 22:33:02 +0200

    modify update driver

--- controllers/driverHome/settings.js
+++ controllers/driverHome/settings.js
@@ -6,6 +6,8 @@ const { Cars } =  require('../../models/cars-model');
 //Helpers
 const { validateUpdateMe } = require("../../validators/settings-validator");
 const { encodeId, hashing } = require("../../helpers/encryption-helper");
+const mail = require("../../services/sendgrid-mail");
+const sms = require("../../services/nexmo-sms");
 
   // Get avarege rate and driver info
   module.exports.getDriver = async (req,res) => {
@@ -47,17 +49,14 @@ const { encodeId, hashing } = require("../../helpers/encryption-helper");
 
   // update the driver info
   module.exports.updateDriver = async (req, res) => {
-    
     // Validate update request
     const { error, value } = validateUpdateMe(req.body);
     if (error) return res.status(404).send(error.details[0].message);
     let request = value;
-
     // Format update request
     if (request.mail) request.mail_verified = false;
     if (request.phone) request.phone_verified = false;
     if (request.pass) request.pass = await hashing(request.pass);
-
     if (request.firstName) {
       let name = {};
       name.first = request.firstName;
@@ -65,10 +64,75 @@ const { encodeId, hashing } = require("../../helpers/encryption-helper");
       delete request.lastName;
       delete request.firstName;
       request.name = name;
+      let profilePic = {};
+      profilePic.data = request.profilePicBuffer;
+      profilePic.contentType = "image/png";
+      request.profilePic = profilePic;
     }
+    await Drivers.findByIdAndUpdate(req.user._id, request);
+    return res.sendStatus(200);
+  };
 
-    await Drivers.findByIdAndUpdate('5ee2490b9b6b5f4018c75f82', request);
-    console.log(request)
+    
+  // Request mail verification
+  module.exports.requestMailVerification = async (req, res) => {
+    //check if email exists.
+    error = await Drivers.findOne({
+      mail: req.body.mail,
+      _id: { $ne: req.user._id },
+    });
+    if (error) return res.status(409).send("email already exists.");
+    // Update mail and set to unverified
+    const userUpdate = await Drivers.findByIdAndUpdate(req.user._id, {
+      mail: req.body.mail,
+      mail_verified: false,
+    });
+    // Send mail with confirmation code & link
+    const code = Number.parseInt(
+      Math.random() * (999999 - 100000) + 100000
+    ).toString();
+
+    const link = `http://localhost:4200/confirm-mail/${encodeId(req.user._id)}`;
+
+    http: await mail.sendVerificationCode(req.body.mail, {
+      code,
+      link,
+      firstName: userUpdate.name.first,
+    });
+
+    // Respond with verification code
+    res.send(link);
+  };
+
+  // Confirm mail
+  module.exports.confirmMail = async (req, res) => {
+    await Drivers.findByIdAndUpdate(req.user._id, {
+      mail_verified: true,
+    });
+    return res.sendStatus(200);
+  };
+
+  // Request phone verification
+  module.exports.requestPhoneVerification = async (req, res) => {
+    // Update phone and set to unverified
+    const userUpdate = await Drivers.findByIdAndUpdate(req.user._id, {
+      phone: req.body.phone,
+      phone_verified: false,
+    });
+
+    // Send an sms with the generated code
+    const code = Number.parseInt(
+      Math.random() * (999999 - 100000) + 100000
+    ).toString();
+    await sms.sendVerificationCode(req.body.phone, code);
+    return res.send(code);
+  };
+
+  // Confirm phone
+  module.exports.confirmPhone = async (req, res) => {
+    await Drivers.findByIdAndUpdate(req.user._id, {
+      phone_verified: true,
+    });
     return res.sendStatus(200);
   };
 
@@ -94,13 +158,13 @@ const { encodeId, hashing } = require("../../helpers/encryption-helper");
       let car = {
         _cars : newCar._id
       };
-      await Drivers.update({_id: "5ee2490b9b6b5f4018c75f82"}, {$push: car});
+      await Drivers.update({_id: req.user._id}, {$push: car});
     }
 
     let car = {
       _cars : isExisted._id
     }
     
-    await Drivers.update({_id: "5ee2490b9b6b5f4018c75f82"}, {$push: car});
+    await Drivers.update({_id: req.user._id}, {$push: car});
     return res.sendStatus(200);
   };
