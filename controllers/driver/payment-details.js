bd78d64d0bbd411f1d8c7c0b147f9396619d6fb8
asmaafoda <asmaafoda271197@gmail.com>
2020-06-27 00:31:39 +0200

    D_History &balance

--- controllers/driver/payment-details.js
+++ controllers/driver/payment-details.js
@@ -1,51 +1,46 @@
 const { Drivers } = require("../../models/drivers-model");
 const { PastTrips } = require("../../models/past-trips-model");
 const { getExpectedTwilioSignature } = require("twilio/lib/webhooks/webhooks");
-
+const { string } = require("@hapi/joi");
+const { intersection } = require("lodash");
+var history,history2,history3;
 module.exports.d_balance = async (req, res) => {
   const viewbalance = await Drivers.findById(req.body.id)
     .select("-_id balance ");
   if (!viewbalance) return res.status(404).send("error");
   res.send(viewbalance) ;
-};
+};  
 
 /////////////////////////////////////
-
-module.exports.d_history  = async (req, res) => {
-    let count;
-    var tn =new Date (Date.now()- 7* 60 * 60 * 24 * 1000);
-    const viewpast = await PastTrips.find({_driver:req.body.id}&&{start:{$gte:tn}})
-      .select("-_id start seats price");
-    if (!viewpast) return res.status(404).send("error");
-    for(let i=0;i<viewpast.length;i++){
-      count=0;
-      for(let j=0;j<viewpast[i].seats.length;j++){
-      count+= viewpast[i].seats[j];}
-    viewpast[i].price=viewpast[i].price*count;
-    viewpast[i].seats=count;
-    };
-
-    res.send( viewpast);
-  };
-  //////////////////////////////////
-  module.exports.b_history= async (req, res) => {
-    let count;
-    const viewbalance = await Drivers.findById(req.body.id)
-       .select("-_id balance ");
-    if (!viewbalance) return res.status(404).send("error");
-
-    var tn =new Date (Date.now()- 7* 60 * 60 * 24 * 1000);
-     const viewpast = await PastTrips.find({_driver:req.body.id}&&{start:{$gte:tn}})
-       .select("-_id start seats price");
-     if (!viewpast) return res.status(404).send("error");
-     for(let i=0;i<viewpast.length;i++){
-       count=0;
-       for(let j=0;j<viewpast[i].seats.length;j++){
-       count+= viewpast[i].seats[j];}
-     viewpast[i].price=viewpast[i].price*count;
-     viewpast[i].seats=count;
-     };
- 
-     res.send([viewbalance,viewpast]);
-   };
-  
+module.exports.d_history = async (req, res) => {
+  let s,c;
+  var tn =new Date (Date.now()- 7* 60 * 60 * 24 * 1000);
+  const viewpast = await Drivers.findById(req.body.id)
+    .select("-_id _tours")
+    .populate({
+      path: "_tours",
+      match: { date: { $gte:tn } },
+      select: " -_id _line date _trips"
+    , populate: [
+      { path: "_line", select: "-_id from to" },
+      { path: "_trips",select: "-_id cost seats "} ]
+    });
+  if (!viewpast) return res.status(404).send("error");
+  history=viewpast._tours;
+  console.log(viewpast);
+  history2={};
+  history3=[];
+  for(let i=0;i<history.length;i++){
+    let s=0,c=0,history2={};
+           for(let j=0;j<history[i]._trips.length;j++){
+               s+=history[i]._trips[j].seats;
+               c+=history[i]._trips[j].cost;}
+                history2.date=history[i].date;
+                history2.line=history[i]._line;
+                history2.cost=c;
+                history2.seats=s;
+                console.log(history2);
+                history3.push(history2);
+              };
+res.send(history3);
+};
--- routes/driver/payment.js
+++ routes/driver/payment.js
@@ -4,12 +4,11 @@ const router = express.Router();
 // Middlewares
 const authentication = require("../../middlewares/authentication");
 // Controllers
-const {d_balance,d_history,b_history} = require("../../controllers/driver/payment-details");
+const {d_balance,d_history} = require("../../controllers/driver/payment-details");
 //------------
 
 router.post("/mybalance", d_balance);//req with id to get balance
 router.post("/myhistory", d_history);//req with id to get history
-router.post("/b_history", b_history);//req with id to get balance & history
 
 
 module.exports = router;
